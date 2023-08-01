386d861bf47833e82140b530648b8db87bf5cc19
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-01 18:00:13 +0200

    Remove unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,3 @@
 export const defaultPort = 3000
 
-export const openAIConversationIDHeader = 'openai-conversation-id'
-export const openAIUserIDHeader = 'openai-ephemeral-user-id'
-
-export const textPlainMIME = 'text/plain; charset=UTF-8'
-
 export const maxSessionLength = 600 // 600 seconds = 10 minutes
